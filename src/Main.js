ab0da4f7540dc3baa2fb036e4e6b6c2d7570a7bf
nohjunh <nsh042777@naver.com>
2022-09-11 19:35:18 +0900

    404 Page using Route path="*"

--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,7 @@ import CourseListPage from "./pages/CourseListPage";
 import WishlistPage from "./pages/WishlistPage";
 import QuestionListPage from "./pages/QuestionListPage";
 import QuestionPage from "./pages/QuestionPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function Main() {
   // 아래 Route path=":courseSlug(경로 파라미터)"를 통해 경로에서 사용하는 동적인 값인 courseSlug라는 변수를 파라미터로써 useParams()를 이용해 동적인 경로로 사용하고 있다.
@@ -18,11 +19,12 @@ function Main() {
             <Route index element={<CourseListPage />} />
             <Route path=":courseSlug" element={<CoursePage />} />
           </Route>
-          <Route path="wishlist" element={<WishlistPage />} />
           <Route path="questions">
             <Route index element={<QuestionListPage />} />
-            <Route path="616825" element={<QuestionPage />} />
+            <Route path=":questionId" element={<QuestionPage />} />
           </Route>
+          <Route path="wishlist" element={<WishlistPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
